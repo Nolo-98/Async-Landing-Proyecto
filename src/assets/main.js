7310cc6da17f408382c2fc9262e6f699e3fff3b9
Nolo-98 <twisterfast@gmail.com>
2023-03-25 15:34:43 -0600

    Desplegando el proyecto

--- src/assets/main.js
+++ src/assets/main.js
@@ -5,66 +5,39 @@ const content = null || document.getElementById('content');
 const options = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': 'e8aaa04226msh47b9fa9bdb28da6p168898jsnb94bf5d0a54d',
+		'X-RapidAPI-Key': 'bd82325e19msh666e08229e0ec1cp1b07d5jsn151b0a974bb7',
 		'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
 	}
 };
 
-async function fetchData(urlApi) {
-
+async function fetchData( urlApi ) {
     const response = await fetch(urlApi, options);
-  
     const data = await response.json();
-  
     return data;
-  
-  }
-  
-  (async () => {
-  
-    try {
-  
-      const videos = await fetchData(API);
-  
-      let view = `
-  
-      ${videos.items.map(video => `
-  
-        <div class="group relative">
-  
-          <div
-  
-            class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
-  
-            <img src="${video.snippet.thumbnail.high.url}" alt="${video.snippet.description}" class="w-full">
-  
-          </div>
-  
-          <div class="mt-4 flex justify-between">
-  
-            <h3 class="text-sm text-gray-700">
-  
-              <span aria-hidden="true" class="absolute inset-0"></span>
-  
-              ${video.snippet.title}
-  
-            </h3>
-  
-          </div>
-  
-        </div>
-  
-      `).slice(0,4).join('')}
-  
-  
-  
-      `;
-  
-    } catch {
-  
-  
-  
-  
+}
+
+(async() => {
+    try{
+        const videos = await fetchData(API);
+        let view = `
+        ${videos.items.map(video => `
+            <div class="group relative">
+              <div
+                class="w-full bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:aspect-none">
+                <img src="${video.snippet.thumbnails.high.url}" alt="${video.snippet.description}" class="w-full">
+              </div>
+              <div class="mt-4 flex justify-between">
+                <h3 class="text-sm text-gray-700">
+                  <span aria-hidden="true" class="absolute inset-0"></span>
+                  ${video.snippet.title}
+                </h3>
+              </div>
+            </div>
+        `).slice(0,4).join('')}
+         
+        `;
+        content.innerHTML = view;
+    }catch(error){
+        console.log(error);
     }
-  
-  })();
+})();
